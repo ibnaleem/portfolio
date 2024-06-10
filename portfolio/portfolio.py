4c93e97f5e0505bc8c01bed34e497bb6eaaff827
ibnaleem <ibnaleem@outlook.com>
2024-06-10 21:20:29 +0100

    add routing, theme, and tab title

--- portfolio/portfolio.py
+++ portfolio/portfolio.py
@@ -1,36 +1,10 @@
-"""Welcome to Reflex! This file outlines the steps to create a basic app."""
-
-from rxconfig import config
-
 import reflex as rx
 
-docs_url = "https://reflex.dev/docs/getting-started/introduction/"
-filename = f"{config.app_name}/{config.app_name}.py"
 
-
-class State(rx.State):
-    """The app state."""
-
-
+@rx.page(route="/", title="Shaffan's Portfolio")
 def index() -> rx.Component:
-    return rx.center(
-        rx.theme_panel(),
-        rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
-            rx.text("Get started by editing ", rx.code(filename)),
-            rx.button(
-                "Check out our docs!",
-                on_click=lambda: rx.redirect(docs_url),
-                size="4",
-            ),
-            rx.logo(),
-            align="center",
-            spacing="7",
-            font_size="2em",
-        ),
-        height="100vh",
-    )
+    return rx.center()
 
-
-app = rx.App()
+app = rx.App(
+    theme=rx.theme(appearance="dark", has_background=True, radius="large", accent_color="teal"))
 app.add_page(index)
