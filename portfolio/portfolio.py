26e7993da45ffb3b7bc5fa7bd38b40b5924b3fd0
ibnaleem <ibnaleem@outlook.com>
2024-06-10 22:25:57 +0100

    add pgp page to main python file

--- portfolio/portfolio.py
+++ portfolio/portfolio.py
@@ -1,10 +1,10 @@
 import reflex as rx
-
+from .pages.pgp import pgp
 
 @rx.page(route="/", title="Shaffan's Portfolio")
 def index() -> rx.Component:
     return rx.center()
 
-app = rx.App(
-    theme=rx.theme(appearance="dark", has_background=True, radius="large", accent_color="teal"))
+app = rx.App(theme=rx.theme(appearance="dark", has_background=True, radius="large", accent_color="teal"))
 app.add_page(index)
+app.add_page(pgp)
